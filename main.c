1d303889c70913382254933f52b5b56bf1484953
Squantor <Squantor@server.fake>
2014-01-24 21:12:25 +0100

    * some minor style changes, they where cramping my style * added authentication and hidden host command support * modified inih to accept #channels as it parsed #something as a comment Use ; for that now

--- main.c
+++ main.c
@@ -5,24 +5,35 @@
 #include <netdb.h>
 #include <stdarg.h>
 #include "./inih/ini.h"
-typedef struct
-{
-	const char* server;
-	int port;
-	const char* channel;
-	const char* nick;
-}irc_config;
+
+#include "nickVarBot.h"
 
 static int config_handler(void* cfg, const char* section,  const char* name, const char* value)
 {
-	irc_config* pconfig = (irc_config*)cfg;
-	#define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
+	nickVarBotState* pconfig = (nickVarBotState*)cfg;
 	if(MATCH("irc","server"))
 	{
 		pconfig->server = strdup(value);
-	} else if (MATCH("irc","nick"))
+	}
+	else if (MATCH("irc","port"))
+	{
+		pconfig->port = strdup(value);
+	}
+	else if (MATCH("irc","nick"))
 	{
 		pconfig->nick = strdup(value);
+	}
+	else if (MATCH("irc","channel"))
+	{
+		pconfig->channel = strdup(value);
+	}
+	else if (MATCH("irc","auth"))
+	{
+	    pconfig->auth = strdup(value);
+	}
+	else if (MATCH("irc","postAuthMode"))
+	{
+	    pconfig->postAuthMode = strdup(value);
 	}
 	else
 	{
@@ -44,10 +55,8 @@ void raw(char *fmt, ...) {
 }
 
 int main() {
-    irc_config config;
-
-    char *channel = "#eindbaas";
-    char *port = "6667";
+    nickVarBotState config;
+    config.botIrcState = disconnected;
 
     char *user, *command, *where, *message, *sep, *target;
     int i, j, l, sl, o = -1, start, wordcount;
@@ -56,18 +65,18 @@ int main() {
     if(ini_parse("nickvarbot.cfg" , config_handler, &config) < 0)
     {
         printf("Can't load 'nickvarbot.cfg'\r\n");
-	return 1;
+        return 1;
     }
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
-    getaddrinfo(config.server, port, &hints, &res);
+    getaddrinfo(config.server, config.port, &hints, &res);
     conn = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
     connect(conn, res->ai_addr, res->ai_addrlen);
-    
+
     raw("USER %s 0 0 :%s\r\n", config.nick, config.nick);
     raw("NICK %s\r\n", config.nick);
-    
+
     while ((sl = read(conn, sbuf, 512))) {
         for (i = 0; i < sl; i++) {
             o++;
@@ -76,9 +85,9 @@ int main() {
                 buf[o + 1] = '\0';
                 l = o;
                 o = -1;
-                
+
                 printf(">> %s", buf);
-                
+
                 if (!strncmp(buf, "PING", 4)) {
                     buf[1] = 'O';
                     raw(buf);
@@ -94,32 +103,46 @@ int main() {
                                 case 2: command = buf + start; break;
                                 case 3: where = buf + start; break;
                             }
-                            if (j == l - 1) continue;
+                            if (j == l - 1)
+                                continue;
                             start = j + 1;
                         } else if (buf[j] == ':' && wordcount == 3) {
                             if (j < l - 1) message = buf + j + 1;
-                            break;
+                                break;
                         }
                     }
-                    
-                    if (wordcount < 2) continue;
-                    
-                    if (!strncmp(command, "001", 3) && channel != NULL) {
-                        raw("JOIN %s\r\n", channel);
-                    } else if (!strncmp(command, "PRIVMSG", 7) || !strncmp(command, "NOTICE", 6)) {
-                        if (where == NULL || message == NULL) continue;
-                        if ((sep = strchr(user, '!')) != NULL) user[sep - user] = '\0';
-                        if (where[0] == '#' || where[0] == '&' || where[0] == '+' || where[0] == '!') target = where; else target = user;
+
+                    if (wordcount < 2)
+                        continue;
+
+                    // we are connected (timestamp 001), spam everything we got!
+                    if  (!strncmp(command, "001", 3) && config.channel != NULL)
+                    {
+                        if(config.auth != NULL);
+                           raw("%s\r\n", config.auth);
+                        if(config.postAuthMode != NULL)
+                            raw("%s\r\n", config.postAuthMode);
+                        raw("JOIN %s\r\n", config.channel);
+                    }
+                    // dissect all messages
+                    else if (!strncmp(command, "PRIVMSG", 7) || !strncmp(command, "NOTICE", 6))
+                    {
+                        if (where == NULL || message == NULL)
+                            continue;
+                        if ((sep = strchr(user, '!')) != NULL)
+                            user[sep - user] = '\0';
+                        if (where[0] == '#' || where[0] == '&' || where[0] == '+' || where[0] == '!')
+                            target = where;
+                        else
+                            target = user;
                         printf("[from: %s] [reply-with: %s] [where: %s] [reply-to: %s] %s", user, command, where, target, message);
-                        //raw("%s %s :%s", command, target, message); // If you enable this the IRCd will get its "*** Looking up your hostname..." messages thrown back at it but it works...
+                        // here the monitoring magic happens
                     }
                 }
-                
             }
         }
-        
     }
-    
+
     return 0;
-    
+
 }
---
+++ nickVarBot.h
@@ -0,0 +1,37 @@
+/*
+    nickname variable bot
+
+    Various macros, structs and other stuff
+
+*/
+#ifndef __NICKVARBOT_H__
+#define __NICKVARBOT_H__
+
+#define MATCH(s, n) (strcmp(section, s) == 0 && strcmp(name, n) == 0)
+
+typedef struct
+{
+
+} ircEvent;
+
+typedef enum
+{
+    disconnected,
+    connected,
+    authed,
+    moded,
+    joined,
+} ircState;
+
+typedef struct
+{
+    ircState    botIrcState;
+	const char* server;
+	const char* port;
+	const char* channel;
+	const char* nick;
+	const char* auth;
+	const char* postAuthMode;
+} nickVarBotState;
+
+#endif
