de24b8a5208cd9a5964b586be4a60a93957e7820
Squantor <Squantor@server.fake>
2014-01-26 12:58:49 +0100

    * added basic var parsing and adding to struct TODO: state save and operand parsing

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctype.h>
 #include <netdb.h>
 #include <stdarg.h>
 #include "./inih/ini.h"
@@ -44,8 +45,10 @@ static int config_handler(void* cfg, const char* section,  const char* name, con
 
 int conn;
 char sbuf[512];
+nickvarNick currentnickvars[MAXVARCOUNT];
 
 void raw(char *fmt, ...) {
+    // TODO flood protection
     va_list ap;
     va_start(ap, fmt);
     vsnprintf(sbuf, 512, fmt, ap);
@@ -59,9 +62,13 @@ int main() {
     config.botIrcState = disconnected;
 
     char *user, *command, *where, *message, *sep, *target;
-    int i, j, l, sl, o = -1, start, wordcount;
+    int i, j, k, l, sl, o = -1, start, wordcount;
     char buf[513];
     struct addrinfo hints, *res;
+    // TODO create a .nickVarBot dir for config and state storage
+    memset(currentnickvars, 0, sizeof currentnickvars);
+    // TODO load current vars from config file
+
     if(ini_parse("nickvarbot.cfg" , config_handler, &config) < 0)
     {
         printf("Can't load 'nickvarbot.cfg'\r\n");
@@ -77,21 +84,27 @@ int main() {
     raw("USER %s 0 0 :%s\r\n", config.nick, config.nick);
     raw("NICK %s\r\n", config.nick);
 
-    while ((sl = read(conn, sbuf, 512))) {
-        for (i = 0; i < sl; i++) {
+    while ((sl = read(conn, sbuf, 512)))
+    {
+        for (i = 0; i < sl; i++)
+        {
             o++;
             buf[o] = sbuf[i];
-            if ((i > 0 && sbuf[i] == '\n' && sbuf[i - 1] == '\r') || o == 512) {
+            if ((i > 0 && sbuf[i] == '\n' && sbuf[i - 1] == '\r') || o == 512)
+            {
                 buf[o + 1] = '\0';
                 l = o;
                 o = -1;
 
                 printf(">> %s", buf);
 
-                if (!strncmp(buf, "PING", 4)) {
+                if (!strncmp(buf, "PING", 4))
+                {
                     buf[1] = 'O';
                     raw(buf);
-                } else if (buf[0] == ':') {
+                }
+                else if (buf[0] == ':')
+                {
                     wordcount = 0;
                     user = command = where = message = NULL;
                     for (j = 1; j < l; j++) {
@@ -122,7 +135,8 @@ int main() {
                            raw("%s\r\n", config.auth);
                         if(config.postAuthMode != NULL)
                             raw("%s\r\n", config.postAuthMode);
-                        raw("JOIN %s\r\n", config.channel);
+                        if(config.channel != NULL)
+                            raw("JOIN %s\r\n", config.channel);
                     }
                     // dissect all messages
                     else if (!strncmp(command, "PRIVMSG", 7) || !strncmp(command, "NOTICE", 6))
@@ -141,17 +155,130 @@ int main() {
                         char *token;
                         strncpy(messagebuf, message, sizeof(messagebuf));
                         token = strtok(messagebuf," ");
-                        // message emitted from channel
-                        if(target[0] == '#')
+                        if(!strncmp(token, "!help", 4))
                         {
-                            if(!strncmp(token, "!hey", 4))
-                            {
-                                raw("PRIVMSG %s :hey yourself %s\r\n", target, user);
-                            }
+                            raw("PRIVMSG %s : valid commands !varlist, !varsave, !vardel\r\n", target, user);
                         }
+                        // list functions
+                        else if(!strncmp(token, "!varlist", 8))
+                        {
+                            int nickcount = 0;
+                            // list all nickname variables
+                            for(j = 0; j < MAXVARCOUNT; j++)
+                            {
+                                if(strlen(currentnickvars[j].name) != 0)
+                                {
+                                    nickcount++;
+                                    raw("PRIVMSG %s :nick %s present\r\n",
+                                                target,
+                                                currentnickvars[j].name
+                                    );
+                                    for(k = 0; k < MAXVARCOUNT; k++)
+                                    {
+                                        if(strlen(currentnickvars[j].namevars[k].name) != 0)
+                                        {
+                                            raw("PRIVMSG %s :nick %s has variable %s=%d used %d times\r\n",
+                                                target,
+                                                currentnickvars[j].name,
+                                                currentnickvars[j].namevars[k].name,
+                                                currentnickvars[j].namevars[k].namevalue,
+                                                currentnickvars[j].namevars[k].nameused
+                                            );
+                                        }
+                                    }
+                                }
+                            }
+                            raw("PRIVMSG %s :nickcount is %d\r\n", target, nickcount);
+                        }
+                        else if(!strncmp(token, "!varsave", 8))
+                        {
+                            // save all nickname variables
+                        }
+                        // parse all incoming lines
                         else
                         {
+                            char *dotptr = strchr(message, '.');
+                            if(dotptr != NULL)
+                            {
+                                if(isalnum(*(dotptr+1)) && isalnum(*(dotptr-1)))
+                                {
+                                    char *nickptr = dotptr;
+                                    char *varptr = dotptr;
+                                    char currnick[MAXVARNAMESIZE];
+                                    char currvar[MAXVARNAMESIZE];
+                                    nickvarNick *currentnickentry = NULL;
+                                    nickvarVar *currentvarentry = NULL;
+                                    memset(currnick, 0, sizeof currnick);
 
+                                    do
+                                        nickptr--;
+                                    while(isalnum(*(nickptr-1)));
+                                    strncpy(currnick, nickptr, dotptr - nickptr);
+
+                                    for(j = 0; j < MAXVARCOUNT; j++)
+                                    {
+                                        if(strlen(currentnickvars[j].name) != 0)
+                                        {
+                                            if(!strcmp(currentnickvars[j].name, currnick))
+                                            {
+                                                currentnickentry = &currentnickvars[j];
+                                                break;
+                                            }
+                                        }
+                                        else
+                                            // keep an empty entry for later
+                                            currentnickentry = &currentnickvars[j];
+                                    }
+                                    if(j == MAXVARCOUNT)
+                                    {
+                                        if(currentnickentry != NULL)
+                                        {
+                                            raw("PRIVMSG %s : adding nickname %s \r\n", target, currnick);
+                                            strncpy(currentnickentry->name, currnick, MAXVARNAMESIZE);
+                                        }
+                                        else
+                                        {
+                                            raw("PRIVMSG %s : cant add nickname %s, full! \r\n", target, currnick);
+                                            continue;
+                                        }
+                                    }
+
+                                    do
+                                        varptr++;
+                                    while(isalnum(*(varptr+1)));
+                                    strncpy(currvar, dotptr+1, varptr - dotptr);
+                                    printf("currvar %s strlen %d\n", currvar, strlen(currvar));
+
+                                    for(j = 0; j < MAXVARCOUNT; j++)
+                                    {
+                                        if(strlen(currentnickentry->namevars[j].name) != 0)
+                                        {
+                                            if(!strcmp(currentnickentry->namevars[j].name, currvar))
+                                            {
+                                                currentvarentry = &currentnickentry->namevars[j];
+                                                break;
+                                            }
+                                        }
+                                        else
+                                            // keep an empty entry for later
+                                            currentvarentry = &currentnickentry->namevars[j];
+                                    }
+                                    if(j == MAXVARCOUNT)
+                                    {
+                                        if(currentvarentry != NULL)
+                                        {
+                                            raw("PRIVMSG %s : adding var %s \r\n", target, currvar);
+                                            strncpy(currentvarentry->name, currvar, MAXVARNAMESIZE);
+                                        }
+                                        else
+                                        {
+                                            printf("PRIVMSG %s : cant add var %s, full!\r\n", target, currvar);
+                                            raw("PRIVMSG %s : cant add var %s, full!\r\n", target, currvar);
+                                            continue;
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
                 }
--- nickVarBot.h
+++ nickVarBot.h
@@ -9,10 +9,24 @@
 
 #define MATCH(s, n) (strcmp(section, s) == 0 && strcmp(name, n) == 0)
 
+#define MAXVARNAMESIZE      32
+#define MAXVARCOUNT         10
+
+// nickname variable data
 typedef struct
 {
+    char name[MAXVARNAMESIZE];
+    int namevalue;
+    // how often a name is used, used by LRU algo for removal
+    int nameused;
+} nickvarVar;
 
-} ircEvent;
+// nickname data
+typedef struct
+{
+    char name[MAXVARNAMESIZE];
+    nickvarVar namevars[MAXVARCOUNT];
+} nickvarNick;
 
 typedef enum
 {
